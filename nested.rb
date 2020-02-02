134f06e7783c1361c3869b63f9c7ae04ba72244f
james Johnson <2013matthew2013@gmail.com>
2020-02-02 15:24:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -23,7 +23,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = {
+	programmer_hash = 
 	  
         :grace_hopper => {
           :known_for => "COBOL",
@@ -41,7 +41,7 @@ def alan_kay_is_known_for
         :alan_kay =>
          :known_for => [] 
   }
-	}
+	
 end
 alan_kay_is_known_for
 
