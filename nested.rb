25c88bc7d75741c70630e74c7dedb7b1eec32aa9
james Johnson <2013matthew2013@gmail.com>
2020-02-02 15:32:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -143,6 +143,7 @@ def adding_to_dennis
         }
      }
 programmer_hash=
-      :dennis_ritchie[:languages] << "Assembly"
+      :dennis_ritchie
+      :languages << "Assembly"
   programmer_hash
 end
