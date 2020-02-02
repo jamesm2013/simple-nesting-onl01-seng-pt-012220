c137fc4558c6941deff2b1b8ef7755e2d42cd014
james Johnson <2013matthew2013@gmail.com>
2020-02-02 15:09:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,13 +82,16 @@ def adding_matz
           :languages => ["C"]
         }
          programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
+    
+    :known_for "Ruby",
     languages:
       ["LISP", "C"]
      }
 programmer_hash
     
 end
+
+adding_matz
 
 def changing_alan
 	programmer_hash = 
