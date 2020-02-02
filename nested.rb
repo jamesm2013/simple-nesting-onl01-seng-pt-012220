19bddef2c1974c1400f0246552da273deeeea0e8
james Johnson <2013matthew2013@gmail.com>
2020-02-02 14:04:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,47 +1,44 @@
+def  nested hash
+end  
 
-def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
+def hopper 
+  programmer_hash => { 
+       	  :grace_hopper => "hopper", {
+          :known_for => ["COOL"],
           :languages => ["COBOL", "FORTRAN"]
-        },
+	      },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => ["Object Orientation"],
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => "Unix",
+          :known_for => ["Unix"],
           :languages => ["C"]
         }
-     }
-
-
+ 	  }
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => ["Object Orientation"],
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+  }
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
