c6c5311b28159ebf7988ecc4354f57df00e3272d
james Johnson <2013matthew2013@gmail.com>
2020-02-02 15:45:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -25,23 +25,23 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
 	  
-        :grace_hopper => 
+        :grace_hopper =>{ 
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        
-        :alan_kay => 
+        }
+        :alan_kay => {
           :known_for => ["Object Orientation"],
           :languages => ["Smalltalk", "LISP"]
-        
+        }
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        
-  programmer_hash={
-        :alan_kay =>
+        }
+  programmer_hash= {
+        :alan_kay =>{
          :known_for => [""] 
+        }
   }
-	
 end
 alan_kay_is_known_for
 
@@ -84,7 +84,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-  programmer_hash ={
+  programmer_hash = {
         :yukihiro_matsumoto => {
          :known_for =>  "Ruby",
          :languages => ["LISP", "C"]
@@ -144,7 +144,9 @@ def adding_to_dennis
         }
      }
 programmer_hash =
-      :dennis_ritchie =>
-      :languages => << "Assembly"
+    :dennis_ritchie =>{
+      :languages => 
+    }  
+      << "Assembly"
   programmer_hash
 end
